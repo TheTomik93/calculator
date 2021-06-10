48e1fc5c88eb29b18a9ff26062cf9c316d397ea6
TheTomik93 <TheTomik93@gmail.com>
2021-06-10 23:45:07 +0200

    delete button

--- main.js
+++ main.js
@@ -1,23 +1,54 @@
 let buttons = document.querySelectorAll(".button");
+let numberButtons = document.querySelectorAll(".number");
+let inputDisplay = document.querySelector("#displayBottom");
+let resultDisplay = document.querySelector("#displayTop");
+let pointButton = document.querySelector("#point");
+let clearButton = document.querySelector("#clear");
+let deleteButton = document.querySelector("#delete");
 
-buttons.forEach(button => button.addEventListener("mousedown", mousedown));
-buttons.forEach(button => button.addEventListener("mouseup", mouseup));
+buttons.forEach(button => button.addEventListener("mousedown", buttonDown));
+buttons.forEach(button => button.addEventListener("mouseup", buttonUp));
+buttons.forEach(button => button.addEventListener("mouseout", buttonUp));
+numberButtons.forEach(button => button.addEventListener("click", function() {appendNumber(button.textContent)}));
+pointButton.addEventListener("click", appendPoint);
+clearButton.addEventListener("click", clear);
+deleteButton.addEventListener("click", deleteLast);
 
-function mousedown(){
+function buttonDown(){
     this.style.backgroundColor = "var(--button-down)";
 }
 
-function mouseup(){
-    this.style.backgroundColor = "var(--button-up)"
+function buttonUp(){
+    this.style.backgroundColor = "var(--button-up)";
 }
 
+function appendNumber(number){
+    if (inputDisplay.textContent === "0"){
+        inputDisplay.textContent = number;
+    }
+    else {
+        inputDisplay.textContent += number;
+    }
+}
 
+function appendPoint(){
+    if (inputDisplay.textContent.includes(".")) return;
+    inputDisplay.textContent += ".";
+}
 
+function clear(){
+    if (inputDisplay.textContent === "0"){
+        resultDisplay.textContent = "";
+    }
+    inputDisplay.textContent = "0";
+}
 
-
-
-
-
+function deleteLast(){
+    if (inputDisplay.textContent === "0"){
+        return;
+    }
+    inputDisplay.textContent = inputDisplay.textContent.slice(0, -1); 
+}
 
 
 

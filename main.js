1fcf0a1648ee6d163bc68342f57f968c825f630e
TheTomik93 <TheTomik93@gmail.com>
2021-06-10 21:14:59 +0200

    layout, design, mouseup, mousedown

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+let buttons = document.querySelectorAll(".button");
+
+buttons.forEach(button => button.addEventListener("mousedown", mousedown));
+buttons.forEach(button => button.addEventListener("mouseup", mouseup));
+
+function mousedown(){
+    this.style.backgroundColor = "var(--button-down)";
+}
+
+function mouseup(){
+    this.style.backgroundColor = "var(--button-up)"
+}
+
+
+
+
+
+
+
+
+
+
+
+
 function add(a,b){
     return (a + b);
 }

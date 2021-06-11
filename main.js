166d92f7ecc3d544ab0cbfd160d1d354146bfe93
TheTomik93 <TheTomik93@gmail.com>
2021-06-11 17:50:54 +0200

    vsecko funguje 1.0

--- main.js
+++ main.js
@@ -5,6 +5,13 @@ let resultDisplay = document.querySelector("#displayTop");
 let pointButton = document.querySelector("#point");
 let clearButton = document.querySelector("#clear");
 let deleteButton = document.querySelector("#delete");
+let operatorButtons = document.querySelectorAll(".operator");
+let equalsButton = document.querySelector("#equals");
+
+let numOne = 0;
+let numTwo = 0;
+let operator = "";
+let result = 0;
 
 buttons.forEach(button => button.addEventListener("mousedown", buttonDown));
 buttons.forEach(button => button.addEventListener("mouseup", buttonUp));
@@ -13,6 +20,8 @@ numberButtons.forEach(button => button.addEventListener("click", function() {app
 pointButton.addEventListener("click", appendPoint);
 clearButton.addEventListener("click", clear);
 deleteButton.addEventListener("click", deleteLast);
+operatorButtons.forEach(button => button.addEventListener("click", function(){operatorPressed(button.textContent)}));
+equalsButton.addEventListener("click", equalsPressed);
 
 function buttonDown(){
     this.style.backgroundColor = "var(--button-down)";
@@ -40,6 +49,10 @@ function clear(){
     if (inputDisplay.textContent === "0"){
         resultDisplay.textContent = "";
     }
+    numOne = 0;
+    numTwo = 0;
+    operator = "";
+    resultDisplay.textContent = "";
     inputDisplay.textContent = "0";
 }
 
@@ -50,18 +63,59 @@ function deleteLast(){
     inputDisplay.textContent = inputDisplay.textContent.slice(0, -1); 
 }
 
+function equalsPressed(){
+    numTwo = Number(inputDisplay.textContent);
+    resultDisplay.textContent = `${numOne} ${operator} ${numTwo} =`;
+    calculate();
+    operator = "";
+    inputDisplay.textContent = result;
+}
 
+function operatorPressed(chosenOperator){
+    numTwo = Number(inputDisplay.textContent);
+    inputDisplay.textContent = "";
+    calculate();
+    operator = chosenOperator;
+    resultDisplay.textContent = `${numOne} ${operator}`;
+}
 
+function calculate(){
+    switch (operator){
+        case "":
+            numOne = numTwo;
+            break;
+        case "+": 
+            add(numOne, numTwo);
+            break;
+        case "-": 
+            substract(numOne, numTwo);
+            break;
+        case "*": 
+            multiply(numOne, numTwo);
+            break;
+        case "/": 
+            divide(numOne, numTwo);
+            break;
+        case "=":
+            equals(numOne, numTwo);
+        default:
+            break;
+    }
+};
 
 function add(a,b){
-    return (a + b);
+    result = (a + b);
+    numOne = result;
 }
 function substract(a,b){
-    return (a - b);
+    result = (a - b);
+    numOne = result;
 }
 function multiply(a,b){
-    return (a * b);
+    result = (a * b);
+    numOne = result;
 }  
 function divide(a,b){
-    return (a / b);
+    result = (a / b);
+    numOne = result;
 }
